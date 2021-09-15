0108d2468d07d99004cd3c1f3b3f4a6acf47627f
wesley mbate <wmbate@meliora.tech>
2021-09-15 23:10:51 +0300

    fixed issue where default route was failing on first-time load added responsiveness enhancments on home, navbar & footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,7 @@ import { ProductsComponent } from "./products/products.component";
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
+    component: HomeComponent
   },
   {
     path: 'home',
