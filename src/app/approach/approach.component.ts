85bdfc10e29c4f9802b4f02d111cefc875f8ae4b
wesley mbate <wmbate@meliora.tech>
2022-07-08 15:09:21 +0300

    added fadeIn animation to approach page onInit

--- src/app/approach/approach.component.ts
+++ src/app/approach/approach.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { style, animate, transition, trigger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-approach',
   templateUrl: './approach.component.html',
-  styleUrls: ['./approach.component.scss']
+  styleUrls: ['./approach.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(600, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class ApproachComponent implements OnInit {
 
