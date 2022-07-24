2a30323a407c99fa7fbc809412fecda3ede861ce
wesley mbate <wmbate@meliora.tech>
2022-07-24 23:38:20 +0300

    added a navbar refined page resposnsiveness

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { style, animate, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent implements OnInit {
+
+  showMenu = false;
 
   constructor(
     public router: Router
@@ -15,6 +27,10 @@ export class NavbarComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
   }
 
 }
